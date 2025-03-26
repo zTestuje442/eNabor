35ae3ba92728cd4a63f88769e17ea386d6bc5f5f
ztestuje <ztestuje442@gmail.com>
2025-03-26 14:34:03 +0100

    cookies accept - #2_commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://umbilgoraj.prezentacja.int.zeto.lublin.pl/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByText('Informacja o plikach cookie').first()).toBeVisible();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.locator("#enable_accept").check();
+  
+  await page.locator("#ckaccept").click();
+
+
 });
