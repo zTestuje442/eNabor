1b3bf9e2839ef66645ee70f3ade40b895b4dcde8
Rafal-Dejneka <rafal.dejneka@gmail.com>
2025-03-27 11:11:20 +0100

    Filling out candidate data

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,8 +17,23 @@ import { test, expect } from '@playwright/test';
   await page.getByRole('button', { name: 'Zaloguj się' }).click();
   await page.getByRole('link', { name: 'Wnioski' }).click();
   await page.locator('.index-header-buttons > div > .chevron').click();
-  await page.getByRole('link', { name: 'Przedszkole/Oddział' }).click();
+  await page.getByRole('link', { name: 'Przedszkole/Oddział przedszkolny' }).click();
   await page.getByRole('link', { name: 'Nowy wniosek rekrutacyjny do' }).click();
+  await page.getByRole('textbox', { name: 'Imię *' }).click();
+  await page.getByRole('textbox', { name: 'Imię *' }).fill('Marcin');
+  await page.getByRole('textbox', { name: 'Nazwisko *' }).click();
+  await page.getByRole('textbox', { name: 'Nazwisko *' }).fill('Goblin');
+  await page.getByRole('textbox', { name: 'PESEL *' }).click();
+  await page.getByRole('textbox', { name: 'PESEL *' }).fill('21242927672');
+  await page.getByRole('textbox', { name: 'Miejscowość *' }).click();
+  await page.getByRole('textbox', { name: 'Miejscowość *' }).fill('Lublin');
+  await page.locator('a').filter({ hasText: 'Lublin (woj. LUBELSKIE,' }).click();
+  await page.getByRole('textbox', { name: 'Numer domu *' }).click();
+  await page.getByRole('textbox', { name: 'Numer domu *' }).fill('34');
+  await page.getByRole('textbox', { name: 'Kod pocztowy *' }).click();
+  await page.getByRole('textbox', { name: 'Kod pocztowy *' }).fill('20-387');
+  await page.getByRole('textbox', { name: 'Poczta *' }).click();
+  await page.getByRole('textbox', { name: 'Poczta *' }).fill('Lublin');
 
-
+  await page.getByRole('button', { name: 'Następny etap' }).click();
 });
